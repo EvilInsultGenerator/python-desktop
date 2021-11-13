1a8dcdfeabbf772205767f10f3a85b97ccf2f0d1
iamDyeus <87000693+iamDyeus@users.noreply.github.com>
2021-11-13 12:13:40 +0530

    Created example.py
    A Simple Python Script that generates Insults with the help of Evil Insult API

---
+++ example.py
@@ -0,0 +1,22 @@
+#A simple insult generation script with the help of Evil Insult API
+
+
+def generate_insult():
+    import requests      
+    evil=requests.get(url='https://evilinsult.com/generate_insult.php?lang=en&type=json')
+    data=evil.json()
+    insult=data['insult']
+    print("Insult :",insult)
+    
+
+while True:
+    try:
+        var=input("Generate Insult? (y/n)\n>>")
+        if var=="y":
+            generate_insult()
+        elif var=="n":
+            break
+        else:
+            print("You can't even select between 2 Options! DumbAss!")
+    except:
+        print("Evil Insult Generator is down,you may try again later.")
